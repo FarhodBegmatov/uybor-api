4d447346c42d68d63ede6b7ca20bff312bd28f1f
Farhod Begmatov <farhodktiat@gmail.com>
2021-07-27 16:22:41 +0500

    Create a model which is named Entrance with migration and controller

--- database/migrations/2021_07_27_112120_create_entrances_table.php
+++ database/migrations/2021_07_27_112120_create_entrances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHousesTable extends Migration
+class CreateEntrancesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateHousesTable extends Migration
      */
     public function up()
     {
-        Schema::create('houses', function (Blueprint $table) {
+        Schema::create('entrances', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('address');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateHousesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('houses');
+        Schema::dropIfExists('entrances');
     }
 }
